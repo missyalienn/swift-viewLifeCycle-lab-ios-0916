7744b1dd9c9e4171f40a9df21905df4fb9eb609d
Missy Allan <missyallan@Melissas-MacBook-Air.local>
2016-09-28 10:10:27 -0400

    dice roll

--- FunWithViews/ViewController.swift
+++ FunWithViews/ViewController.swift
@@ -10,17 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+
+    
+    
     @IBAction func dieButtonTapped(_ sender: AnyObject) {
+        let randomNumber = randomDiceRoll()
+        print("Number generated is\(randomNumber)")
+        rearrangeDie()
         
     }
     
+
+    
+    // Write other functions down where randomDiceRoll is, but call them under dieButtonTapped
+    
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+     
+        
     }
     
     // Returns back a random Int (1, 2, 3, 4, 5, or 6)
     func randomDiceRoll() -> Int {
         return Int(arc4random_uniform(6) + 1)
+        
     }
     
+   
+    var count = 1
+    
+    func showRoll(counter: Int, roll: Int ) {
+
+        
+     
+        
+        }
+    
+    }
+    
+    
+
+
+    
+    // show or hide black boxes  based on result of randomDiceRoll function
+    
+
+        
 }
+
